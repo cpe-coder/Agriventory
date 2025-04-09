b8379480f857546498e23a528caab757046aba43
Grian Gajila <griangajila2@gmail.com>
2025-04-09 16:11:37 +0800

    feat: add login view

--- Agriventory/View/LoginView.xaml.cs
+++ Agriventory/View/LoginView.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Input;
 
 namespace Agriventory.View;
 
@@ -7,5 +8,13 @@ public partial class LoginView : Window
     public LoginView()
     {
         InitializeComponent();
+    }
+
+    private void LoginView_OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+        {
+            DragMove();
+        }
     }
 }
