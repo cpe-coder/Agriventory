29d1cde1f06f457cd335bdce3a861f4fe9735fb4
Grian Gajila <griangajila2@gmail.com>
2025-04-29 03:22:13 +0800

    wip

--- Agriventory/View/ChickenView.xaml.cs
+++ Agriventory/View/ChickenView.xaml.cs
@@ -1,4 +1,6 @@
+using System.Windows;
 using System.Windows.Controls;
+using Agriventory.Model;
 
 namespace Agriventory.View;
 
@@ -7,5 +9,37 @@ public partial class ChickenView : UserControl
     public ChickenView()
     {
         InitializeComponent();
+        LoadData();
+    }
+    private void LoadData()
+    {
+        var items = new List<ChickenItem>
+        {
+            new ChickenItem { Number = 1, Product = "Sample A", Stocks = 100, Category = "Sarimanok", DateImported = DateTime.Now },
+            new ChickenItem { Number = 2, Product = "Sample B", Stocks = 80, Category = "Kalmback", DateImported = DateTime.Now },
+            new ChickenItem { Number = 3, Product = "Sample C", Stocks = 60, Category = "Vienovo", DateImported = DateTime.Now.AddDays(-1) },
+            new ChickenItem { Number = 4, Product = "Sample D", Stocks = 150, Category = "Kalmback", DateImported = DateTime.Now.AddDays(-2) }
+        };
+
+        FeedsDataGrid.ItemsSource = items;
+    }
+
+    private void EditButton_Click(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        var selectedItem = (ChickenItem)((FrameworkElement)button).DataContext;
+        MessageBox.Show($"You can now edit {selectedItem.Product}");
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        var selectedItem = (ChickenItem)((FrameworkElement)button).DataContext;
+        MessageBox.Show($"Are you sure you want to delete {selectedItem.Product}?");
     }
 }
+   
+
+
+
+
