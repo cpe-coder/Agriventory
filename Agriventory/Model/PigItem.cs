6b0027dad4ee5f3fb78e294eb5481eccb6a396d0
Grian Gajila <griangajila2@gmail.com>
2025-04-29 03:28:54 +0800

    wip

---
+++ Agriventory/Model/PigItem.cs
@@ -0,0 +1,10 @@
+namespace Agriventory.Model;
+
+public class PigItem
+{
+    public int Number { get; set; }
+    public string Product { get; set; }
+    public int Stocks { get; set; }
+    public string Category { get; set; }
+    public DateTime DateImported { get; set; }
+}
--- Agriventory/View/PigView.xaml.cs
+++ Agriventory/View/PigView.xaml.cs
@@ -1,4 +1,6 @@
+using System.Windows;
 using System.Windows.Controls;
+using Agriventory.Model;
 
 namespace Agriventory.View;
 
@@ -7,5 +9,32 @@ public partial class PigView : UserControl
     public PigView()
     {
         InitializeComponent();
+        LoadData();
+    }
+    private void LoadData()
+    {
+        var items = new List<PigItem>
+        {
+            new PigItem { Number = 1, Product = "Sample A", Stocks = 100, Category = "Uno feeds", DateImported = DateTime.Now },
+            new PigItem { Number = 2, Product = "Sample B", Stocks = 80, Category = "Uhanco", DateImported = DateTime.Now },
+            new PigItem { Number = 3, Product = "Sample C", Stocks = 60, Category = "Cargill", DateImported = DateTime.Now.AddDays(-1) },
+            new PigItem { Number = 4, Product = "Sample D", Stocks = 150, Category = "Feedpro", DateImported = DateTime.Now.AddDays(-2) }
+        };
+
+        FeedsDataGrid.ItemsSource = items;
+    }
+
+    private void EditButton_Click(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        var selectedItem = (ChickenItem)((FrameworkElement)button).DataContext;
+        MessageBox.Show($"You can now edit {selectedItem.Product}");
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        var selectedItem = (ChickenItem)((FrameworkElement)button).DataContext;
+        MessageBox.Show($"Are you sure you want to delete {selectedItem.Product}?");
     }
 }
